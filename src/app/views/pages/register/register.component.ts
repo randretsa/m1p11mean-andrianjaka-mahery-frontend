97b61fcc67465110c47da0caa58c156459a1f332
randretsa <randretsaandrianjaka30@gmail.com>
2024-02-13 09:22:43 +0300

    Code refactoring.

--- src/app/views/pages/register/register.component.ts
+++ src/app/views/pages/register/register.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, inject } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
 import {UserService} from '../../../services/user.services';
 import { IUser } from 'src/app/types/user';
 @Component({
@@ -11,38 +10,27 @@ export class RegisterComponent implements OnInit{
 
   public users: IUser[] = [];
   userService: UserService = inject(UserService);
+  user = {
+    gender: {
+      code: '',
+      name: ''
+  },
+  _id: null,
+  lastName: '',
+  firstName: 'null',
+  birthDate: '',
+  email: '',
+  phoneNumber: '',
+  password: '',
+  privilege: "65c394939fc65f798c79a4cb",
+} 
 
   constructor() { }
 
-  applyForm = new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
-    birthDate: new FormControl(''),
-    gender: new FormControl(''),
-    phoneNumber: new FormControl(''),
-    email: new FormControl(''),
-    password: new FormControl(''),
-    confirmPassword: new FormControl('')
-  });
 
-  submitApplication(){
-    
-    const user = {
-        gender: {
-          code: this.applyForm.value.gender,
-          name: null
-      },
-      _id: null,
-      lastName: this.applyForm.value.lastName,
-      firstName: this.applyForm.value.firstName,
-      birthDate: this.applyForm.value.birthDate,
-      email: this.applyForm.value.email,
-      phoneNumber: this.applyForm.value.phoneNumber,
-      password: this.applyForm.value.password,
-      privilege: "65c394939fc65f798c79a4cb",
-  } 
+  submitApplication(form: any){
 
-    this.userService.register(user)
+    this.userService.register(this.user)
     .subscribe(
       response => console.log('Succes!', response),
       error => console.log('Error!', error),
